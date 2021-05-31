7e09c52563e8f8bcb41a1cd257276d135af58859
PawelPazderski <pawel.pazderski@op.pl>
2021-05-31 14:56:19 +0200

    Home component basics

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react'
+
+
+const Home = () => {
+  
+
+    return (
+    <>
+        <div className="home-container">
+            <h1>Let's do it</h1>
+        </div>
+        
+    </>)
+}
+    
+export default Home
