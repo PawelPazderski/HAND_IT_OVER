c9580e8aef28d54b567ca9ec78fa79f3607a6781
PawelPazderski <pawel.pazderski@op.pl>
2022-07-11 19:41:17 +0200

    Reverting to 513e1ddfbae958b4dc17e649913049d0ec29f357

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMIr9hT7zTYuBS76dD9mndOmJ-JsLTv9I",
+    authDomain: "handitover-52b63.firebaseapp.com",
+    databaseURL: "https://handitover-52b63-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "handitover-52b63",
+    storageBucket: "handitover-52b63.appspot.com",
+    messagingSenderId: "7948359141",
+    appId: "1:7948359141:web:f5cf0db8e45553f1f6cb0c",
+    measurementId: "G-4KVN27E0GH"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+const auth = getAuth(app);
+const storage = getStorage(app)
+
+
+
+
+
+
